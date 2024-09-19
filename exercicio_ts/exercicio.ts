f58a3cd90b9563caa0a3b31ae2089f6285cbd443
Igor Santana <50204020+tligor@users.noreply.github.com>
2024-09-19 16:23:46 -0300

    correcoes para exercicio

--- exercicio_ts/exercicio.ts
+++ exercicio_ts/exercicio.ts
@@ -1,9 +1,11 @@
-let nameUsr:string = 'Igor';
-const saudacao:string = `Olá ${nameUsr}`;
-
+const saudacao:string = 'Olá ';
+let nomeUsr:string = 'Igor';
 function multiplica(a:number,b:number): number{
     return a * b;
 }
+function enviaSaudacao(saudacao:string, nome:string):string{
+    return  saudacao + nome
+}
 
-console.log(multiplica(2,5));
-console.log(saudacao)
+console.log(multiplica(3,5));
+console.log(enviaSaudacao(saudacao, nomeUsr));
