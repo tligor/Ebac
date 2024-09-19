5502eae83a06888607a52db51a0a5614b34bd4a5
Igor Santana <50204020+tligor@users.noreply.github.com>
2024-09-19 15:05:42 -0300

    exercicio TS

---
+++ exercicio_ts/exercicio.ts
@@ -0,0 +1,9 @@
+let nameUsr:string = 'Igor';
+const saudacao:string = `Olá ${nameUsr}`;
+
+function multiplica(a:number,b:number): number{
+    return a * b;
+}
+
+console.log(multiplica(2,5));
+console.log(saudacao)
